f8e9bcd1a2a5342350157211a67f38d5f3c45418
lol0chx <henoksmekuria@gmail.com>
2026-02-12 22:37:30 -0800

    Remove unrelated project folders from repo

--- bookStoreApi/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "df8icafxq",
-  api_key: "218628971993324",
-  api_secret: "PX2IKSROu2jg8nt-pwYaynzXVOw",
-});
-
-module.exports = cloudinary;
--- flutterBookUploaderNewApp/android/app/src/main/kotlin/com/example/BooksUploaderApp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.BooksUploaderApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
